ab42fc7d6d8cd45050374025a718b583f1bd00f0
eberjg <eberjg@gmail.comm>
2018-07-11 22:15:07 -0400

    Initial Commit

---
+++ RsappTests/RsappTests.swift
@@ -0,0 +1,36 @@
+//
+//  RsappTests.swift
+//  RsappTests
+//
+//  Created by Guest03 on 7/11/18.
+//  Copyright © 2018 Guest03. All rights reserved.
+//
+
+import XCTest
+@testable import Rsapp
+
+class RsappTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
