090c2d0bd7a9c5a16f2c8221d662ed3df4277678
Eber <eberjg@gmail.com>
2018-08-12 11:30:47 -0400

    add label outlet for property view

---
+++ Rsapp/RecentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecentViewController.swift
+//  Rsapp
+//
+//  Created by EberG on 8/12/18.
+//  Copyright © 2018 Guest03. All rights reserved.
+//
+
+import UIKit
+
+class RecentViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //MARK: IBaction
+    
+    @IBAction func mixerButtonPressed(_ sender: Any) {
+    }
+    
+    
+
+}
---
+++ Rsapp/propertyCollectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  propertyCollectionViewCell.swift
+//  Rsapp
+//
+//  Created by EberG on 8/12/18.
+//  Copyright © 2018 Guest03. All rights reserved.
+//
+
+import UIKit
+
+class propertyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var likeButtonOutlet: UIButton!
+    
+    @IBOutlet weak var roomLabel: UILabel!
+    
+    @IBOutlet weak var bathroomLabel: UILabel!
+    
+    @IBOutlet weak var parkingLabel: UILabel!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func startButtonPressed(_ sender: Any) {
+    }
+    
+    
+}
